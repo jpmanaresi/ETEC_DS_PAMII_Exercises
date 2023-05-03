ac32e924afe1175c31ec697565a72c7ed0d0835b
jpmanaresi <jpmanaresi@gmail.com>
2023-05-02 22:46:42 -0300

    teste

---
+++ app/src/main/java/com/example/tasktoday/model.Tarefa/Tarefa.kt
@@ -0,0 +1,10 @@
+package com.example.tasktoday.model.Tarefa
+
+class Tarefa(nome: String, detalhes: String?, createdDate: Date, pzoFinal: Date) {
+    var status="pendente"
+        get() {field}
+        set(value) {
+            field = value
+        }
+
+}
